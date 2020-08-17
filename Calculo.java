8440ec72c75e8bf2122be9f52d379aaa7d1740cd
Gui Pongeti <gui22pongeti@gmail.com>
2020-08-17 23:30:55 +0000

    Initial commit

---
+++ Calculo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Calculo{
+
+Scanner ler = new Scanner(System.in);
+    public void leituraDeDados(){
+
+    int num = 0;
+    int num2 = 0;
+    int resul = 0;
+    System.out.println("Digite um número: ");
+    num = ler.nextInt();
+    System.out.println("Digite um número: ");
+    num2 = ler.nextInt();
+    }
+    
+    public calculo void(){
+    resul = num + num2;
+    }
+
+    public void seSenao(){   
+    if (resul >= 50 ){
+    System.out.println("Correta resposta!");
+    }
+    else{
+      System.out.println("Morra...");
+    }
+}
+
+}
